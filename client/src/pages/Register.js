6fe7ed216ca1c28d997f0239f043036800e7ac24
Sachin Raj <sachintk99@gmail.com>
2022-11-15 11:12:06 +0530

    first commit

---
+++ client/src/pages/Register.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Button, Form, Input } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import toast from "react-hot-toast";
+
+function Register() {
+  const navigate = useNavigate();
+  const onFinish = async(values) =>{
+    try {
+      const response = await axios.post('/api/user/register',values)
+      if (response.data.success) {
+        toast.success(response.data.message);
+        toast("Redirecting to login page");
+        navigate("/login");
+      } else {
+        toast.error(response.data.message)
+      }
+    } catch (error) {
+      toast.error("Something went wrong")
+    }
+  }
+
+  return (
+    <div>
+        <div className='authentication'>
+           <div className='authentication-form card p-3'>
+             <h1 className='card-title'>Nice To Meet U</h1>
+             <Form layout='vertical' onFinish={onFinish}>
+                <Form.Item label='Name' name='name'>
+                    <Input placeholder='Name' />
+                </Form.Item>
+                <Form.Item label='Email' name='email'>
+                    <Input placeholder='Email' />
+                </Form.Item>
+                <Form.Item label='Password' name='password'>
+                    <Input placeholder='Password' type='password'/>
+                </Form.Item>
+                <Button className='primary-button my-2' htmlType='submit'>REGISTER</Button>
+              <Link to='/login' className='anchor'>CLICK HERE TO LOGIN</Link>
+             </Form>
+           </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
